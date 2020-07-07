727b07bc95f9d83b0534cd6bf7e131255e5c4d7d
Zhe He <32999626+NightHz@users.noreply.github.com>
2020-07-07 22:05:17 +0800

    较好的移动；较好的跳跃
    移动实现加速减速过程
    跳跃实现可变高，更快下落，二段跳

---
+++ Assets/[NH][P]Better2DGame/Jump.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Better2DGame;
+
+namespace Better2DGame
+{
+    public class Jump : MonoBehaviour
+    {
+        bool jumpStart = false;
+        bool jumpKeep = false;
+        bool standInFloor = false;
+        bool secondJump = true;
+        void UpdateInput()
+        {
+            // jumpStart = GameInput.Jump;
+            jumpStart = Input.GetKeyDown(KeyCode.X);
+            jumpKeep = Input.GetKey(KeyCode.X);
+        }
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (!collision.isTrigger)
+            {
+                standInFloor = true;
+                secondJump = true;
+            }
+        }
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (!collision.isTrigger)
+                standInFloor = false;
+        }
+
+        [Range(0, 8)] public float jumpBasicHeight = 3.4f;
+        [Range(0, 3)] public float jumpBasicTime = 1f;
+        float jumpVelocity = 0;
+        float gravity = 0;
+        public bool doubleJump = true;
+        [Range(0, 10)] public float gravityScaleLowJump = 3f;
+        [Range(0, 10)] public float gravityScaleDoubleJump = 1.4f;
+        [Range(0, 10)] public float gravityScaleFastFalling = 1.3f;
+
+        Rigidbody2D rb;
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            // set global gravity
+            Physics2D.gravity = new Vector2(0, -1);
+            Debug.Log("Jump Script sets 2d gravity to (0,-1)");
+            // set jump velocity and gravity
+            jumpVelocity = 4 * jumpBasicHeight / jumpBasicTime;
+            gravity = 8 * jumpBasicHeight / (jumpBasicTime * jumpBasicTime);
+            rb.gravityScale = gravity;
+        }
+        private void Update()
+        {
+#if UNITY_EDITOR
+            // update jump velocity and gravity
+            jumpVelocity = 4 * jumpBasicHeight / jumpBasicTime;
+            gravity = 8 * jumpBasicHeight / (jumpBasicTime * jumpBasicTime);
+#endif
+
+            UpdateInput();
+
+            // jump
+            if (jumpStart)
+            {
+                // first jump
+                if (standInFloor)
+                {
+                    rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
+                    standInFloor = false;
+                }
+                // second jump
+                else if(doubleJump && secondJump)
+                {
+                    rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
+                    secondJump = false;
+                }
+            }
+            // adjust gravity
+            if (rb.velocity.y > 0)
+            {
+                // 上升中
+                if (jumpKeep)
+                {
+                    // 保持摁下跳跃
+                    if (!doubleJump || secondJump)
+                        // 第一次跳跃
+                        rb.gravityScale = gravity;
+                    else
+                        // 第二次跳跃
+                        rb.gravityScale = gravity * gravityScaleDoubleJump;
+                }
+                else
+                    // 停止摁下条约——低高度跳跃
+                    rb.gravityScale = gravity * gravityScaleLowJump;
+            }
+            else
+            {
+                // 下降中
+                rb.gravityScale = gravity * gravityScaleFastFalling;
+            }
+        }
+    }
+}
---
+++ Assets/[NH][P]Better2DGame/Move.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Better2DGame;
+
+namespace Better2DGame
+{
+    public class Move : MonoBehaviour
+    {
+        public bool xCanMove = true;
+        public bool yCanMove = true;
+        Vector2 moveInput = Vector2.zero;
+        void UpdateInput()
+        {
+            // moveInput = GameInput.Move;
+            if (xCanMove)
+            {
+                if (Input.GetKey(KeyCode.RightArrow))
+                    moveInput.x = 1;
+                else if (Input.GetKey(KeyCode.LeftArrow))
+                    moveInput.x = -1;
+                else
+                    moveInput.x = 0;
+            }
+            if (yCanMove)
+            {
+                if (Input.GetKey(KeyCode.UpArrow))
+                    moveInput.y = 1;
+                else if (Input.GetKey(KeyCode.DownArrow))
+                    moveInput.y = -1;
+                else
+                    moveInput.y = 0;
+            }
+            if (moveInput != Vector2.zero)
+                moveInput.Normalize();
+        }
+
+        Rigidbody2D rb;
+        [Range(0, 20)] public float moveVelocity = 8f;
+        [Range(0, 1)] public float speedUpTime = 0.15f;
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+        private void Update()
+        {
+            UpdateInput();
+
+            // move
+            Vector2 deltaVel = Vector2.zero;
+            if (xCanMove && yCanMove)
+            {
+                deltaVel = moveVelocity * moveInput - rb.velocity;
+            }
+            else if (xCanMove)
+            {
+                deltaVel = new Vector2(moveVelocity * moveInput.x - rb.velocity.x, 0);
+            }
+            else if (yCanMove)
+            {
+                deltaVel = new Vector2(0, moveVelocity * moveInput.y - rb.velocity.y);
+            }
+            if (deltaVel != Vector2.zero)
+            {
+                float change = moveVelocity / speedUpTime * Time.deltaTime;
+                change = Mathf.Min(deltaVel.magnitude, change);
+                rb.velocity += change * deltaVel.normalized;
+            }
+        }
+    }
+}
